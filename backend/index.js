c03ffdd505f5a8f795c01f5a0ef4d50c9c5361a5
Karan Dalakoti <directconnectinside@gmail.com>
2025-03-14 19:02:29 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 const __dirname = path.resolve()
 
 const corstOption = {
-    origin : 'http://localhost:5173',
+    origin : process.env.URL,
     credentials : true
 }
 
